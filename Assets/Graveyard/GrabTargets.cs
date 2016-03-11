6a92ec4533b434860ed83d1b3a98cdfddd5682d5
Alexander <s1deeex@gmail.com>
2016-03-11 23:02:56 +0200

    Added full terrain

---
+++ Assets/Graveyard/GrabTargets.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrabTargets : MonoBehaviour {
+
+	public static bool count = 0;
+	
+	void OnCollisionEnter(Collision collision) {
+		if (collision.gameObject.tag == "Targets") {
+			count++;
+			Destroy (collision.gameObject);
+		}
+	}
+}
